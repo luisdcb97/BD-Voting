b08d69214fcc0136508d94723d7c932135ae0274
Luis <luisdcb97@gmail.com>
2017-12-09 20:22:24 +0000

    Add listas to db

---
+++ SQL files/Fill_Listas.sql
@@ -0,0 +1,14 @@
+INSERT INTO listas(nome)
+VALUES ('C');
+
+INSERT INTO listas(nome, votos)
+VALUES ('J', 20);
+
+INSERT INTO listas(nome, votos, tipo)
+VALUES ('H', 30, 'TEACHER');
+
+INSERT INTO listas(id, nome, votos, tipo)
+VALUES (50, 'NULOS', 50, 'TEACHER');
+
+INSERT INTO listas(nome, votos, tipo, eleicao)
+VALUES ('BRANCOS', 60, 'JANITOR', 2);
